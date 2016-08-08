3913b77b74c3d275914b9627a56d748609292d96
MD Javedul Ferdous <mdjavedulferdous@gmail.com>
2016-08-09 01:12:12 +0600

    Add files via upload

---
+++ Hang_Man_Game/Hang_Man_Game/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace GuessIT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Location = new System.Drawing.Point(401, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(181, 308);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 19);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(181, 286);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(14, 4);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(392, 200);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Missed:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Word Length:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Location = new System.Drawing.Point(14, 203);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox3.Size = new System.Drawing.Size(391, 109);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(259, 56);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 26);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Reset";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(259, 22);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 26);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "About";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 59);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(88, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 24);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(88, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(20, 56);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 26);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Submit Word";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 22);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit Letter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Chance:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 316);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Guess it!";
+            this.Shown += new System.EventHandler(this.fo);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Hang_Man_Game/Hang_Man_Game/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GuessIT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string word = "";
+        List<Label> labels = new List<Label>();
+        int ammount = 0;
+        enum BodyParts 
+        {
+            Head,
+            Left_Eye,
+            Right_Eye,
+            Mouth,
+            Left_Arm,
+            Right_Arm,
+            Body,
+            Left_Leg,
+            Right_Leg
+
+        }
+
+        void DrawHangPost()
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Brown,10);
+            g.DrawLine(p, new Point(130,218),  new Point(130,5));
+            g.DrawLine(p, new Point(135, 5), new Point(65, 5));
+            g.DrawLine(p, new Point(60, 0), new Point(60, 50));
+           /* DrawBodyPart(BodyParts.Head);
+            DrawBodyPart(BodyParts.Left_Eye);
+            DrawBodyPart(BodyParts.Right_Eye);
+            DrawBodyPart(BodyParts.Mouth);
+            DrawBodyPart(BodyParts.Body);
+            DrawBodyPart(BodyParts.Left_Arm);
+            DrawBodyPart(BodyParts.Right_Arm);
+            DrawBodyPart(BodyParts.Left_Leg);
+            DrawBodyPart(BodyParts.Right_Leg);
+            /*GetRandomWord();
+            MessageBox.Show(GetRandomWord());*/
+            Makelabels();
+            
+        }
+
+        void DrawBodyPart(BodyParts bp)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Blue);
+            if(bp==BodyParts.Head)
+            {
+                g.DrawEllipse(p,40,50,40,40);
+            }
+            else if (bp == BodyParts.Left_Eye)
+            {
+                SolidBrush s = new SolidBrush(Color.Black);
+                g.FillEllipse(s, 50, 60, 5, 5);
+            }
+            else if (bp == BodyParts.Right_Eye)
+            {
+                SolidBrush s = new SolidBrush(Color.Black);
+                g.FillEllipse(s, 63, 60, 5, 5);
+            }
+            else if (bp == BodyParts.Mouth)
+            {
+                g.DrawArc(p,50,60,20,20,45,90);
+            }
+            else if (bp == BodyParts.Body)
+            {
+                g.DrawLine(p,new Point(60,90),new Point(60,170));
+            }
+            else if (bp == BodyParts.Left_Arm)
+            {
+                g.DrawLine(p, new Point(60, 100), new Point(30, 85));
+            }
+            else if (bp == BodyParts.Right_Arm)
+            {
+                g.DrawLine(p, new Point(60, 100), new Point(90, 85));
+            }
+            else if (bp == BodyParts.Left_Leg)
+            {
+                g.DrawLine(p, new Point(60, 170), new Point(30, 190));
+            }
+            else if (bp == BodyParts.Right_Leg)
+            {
+                g.DrawLine(p, new Point(60, 170), new Point(90, 190));
+            }
+
+        }
+
+        void Makelabels()
+        {
+            word = GetRandomWord();
+            char[] chars = word.ToCharArray();
+            int between = 330 / chars.Length - 1;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                labels.Add(new Label());
+                labels[i].Location = new Point((i * between) + 10, 80);
+                labels[i].Text= "_";
+                labels[i].Parent = groupBox2;
+                labels[i].BringToFront();
+                labels[i].CreateControl();
+             }
+            label1.Text = "Word Length " + (chars.Length).ToString();
+         }
+
+        string GetRandomWord()
+        {
+            string[] words = new string[] { "diplomacy", "flexible", "imitation", "gigantic","evangelist", "unique", "debut", "transition", "sense", "activity", "eject", "microsoft", "cabinet", "candor", "cat", "beset", "evolution", "transfer", "amity", "annual", "awaken", "azure", "believe", "birth" };
+            Random ran = new Random();
+            return words[ran.Next(0,words.Length-1)];
+        }
+
+        private void fo(object sender, EventArgs e)
+        {
+            DrawHangPost();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char letter = textBox1.Text.ToLower().ToCharArray()[0];
+            textBox1.Text = "";
+            if (!char.IsLetter(letter))
+            {
+                MessageBox.Show("You can only submit a letter","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+            if (word.Contains(letter))
+            {
+                char[] letters = word.ToCharArray();
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    if (letters[i] == letter)
+                        labels[i].Text = letter.ToString();
+                    
+                }
+                foreach (Label l in labels)
+                    if (l.Text == "_") return;
+                MessageBox.Show("You Win!!!","Congratulation");
+                ResetGame();
+            }
+            else
+            {
+                MessageBox.Show("Letter is not correct","Sorry");
+                label2.Text += " " + letter.ToString() + ",";
+                label3.Text ="Chance: "+(8 - ammount).ToString();
+                DrawBodyPart((BodyParts)ammount);
+                ammount++;
+                if (ammount == 9)
+                {
+                    MessageBox.Show("Game Over!!");
+                }
+            }
+        }
+
+        void ResetGame()
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(panel1.BackColor);
+            GetRandomWord();
+            Makelabels();
+            DrawHangPost();
+            label2.Text = "Missed: ";
+            textBox1.Text = "";
+            ammount = 0;
+            label3.Text = "Chance: ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == word)
+            {
+                MessageBox.Show("You Win!!!", "Congratulation");
+                textBox2.Text = "";
+                ResetGame();
+            }
+            else
+            {
+                MessageBox.Show("The Word is not Correct!");
+                textBox2.Text = "";
+                DrawBodyPart((BodyParts)ammount);
+                ammount++;
+                if (ammount == 9)
+                {
+                    MessageBox.Show("Wrong Guess");
+                    ResetGame();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Javedul Ferdous\nEast West University\nBangladesh\nContact:jaf.rakib@live.com","About me");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ResetGame();
+        }
+
+    }
+}
